497d12bcca33fb4f9b7b573de9772d95264a1fe4
trentbarboza <hellajawns@gmail.com>
2022-01-23 17:25:33 -0800

    added lat lon fetch

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,7 @@
 let input = document.getElementById('input');
 let searchBtn = document.getElementById('searchBtn');
-let Icon = document.getElementById('Icon');
+let mainIcon = document.getElementById('mainIcon');
 let Location = document.getElementById('Location');
-let icon = document.getElementById('icon');
 let temp = document.getElementById('temp');
 let desc = document.getElementById('desc');
 let feelsLike = document.getElementById('feelsLike');
@@ -22,10 +21,21 @@ let F2CBTN = document.getElementById('F2CBTN');
 let day1Temp = document.getElementById('day1Temp');
 let day2Temp = document.getElementById('day2Temp');
 let day3Temp = document.getElementById('day3Temp');
+let day4Temp = document.getElementById('day4Temp');
+let day5Temp = document.getElementById('day5Temp');
+let day1Icon = document.getElementById('day1Icon');
+let day2Icon = document.getElementById('day2Icon');
+let day3Icon = document.getElementById('day3Icon');
+let day4Icon = document.getElementById('day4Icon');
+let day5Icon = document.getElementById('day5Icon');
 
 let lat1;
 let lon1;
+let weatherOneCall;
 
+let lat2;
+let lon2;
+let weatherOneCall2;
 
 function getWeather() {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=philadelphia,usa&units=imperial&APPID=99ddde24d75f18e8da53fbebabbdd073")
@@ -33,7 +43,7 @@ function getWeather() {
     .then(data => {
         //console.log(data);
         Location.textContent = data.name + "," + data.sys.country;
-        Icon.textContent = data.weather.icon;
+        mainIcon.src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
         temp.textContent = data.main.temp + "°F";
         feelsLike.textContent = "Feels Like: " + data.main.feels_like + "°F";
         wind.textContent = "Wind: " + data.wind.speed + " mph";
@@ -42,26 +52,50 @@ function getWeather() {
         lat1 = data.coord.lat;
         lon1 = data.coord.lon;
 
-        LatLon
+        weatherOneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat1}&lon=${lon1}&exclude=hourly,minutely&units=imperial&appid=99ddde24d75f18e8da53fbebabbdd073`
 
+        getOneCall(weatherOneCall);
     });
 }
 getWeather();
+
+function getOneCall(weatherOneCall){
+    fetch(weatherOneCall)
+    .then (resp => resp.json())
+    .then (data => {
+        day1Temp.textContent = data.daily[1].temp.max + " / " + data.daily[1].temp.min + "°";
+        day1Icon.src = "https://openweathermap.org/img/wn/" + data.daily[1].weather[0].icon + ".png";
+        day2Temp.textContent = data.daily[2].temp.max + " / " + data.daily[2].temp.min + "°";
+        day2Icon.src = "https://openweathermap.org/img/wn/" + data.daily[2].weather[0].icon + ".png";
+        day3Temp.textContent = data.daily[3].temp.max + " / " + data.daily[3].temp.min + "°";
+        day3Icon.src = "https://openweathermap.org/img/wn/" + data.daily[3].weather[0].icon + ".png";
+        day4Temp.textContent = data.daily[4].temp.max + " / " + data.daily[4].temp.min + "°";
+        day4Icon.src = "https://openweathermap.org/img/wn/" + data.daily[4].weather[0].icon + ".png";
+        day5Temp.textContent = data.daily[5].temp.max + " / " + data.daily[5].temp.min + "°";
+        day5Icon.src = "https://openweathermap.org/img/wn/" + data.daily[5].weather[0].icon + ".png";
+    })
+}
+
 
 function get5DayWeather() {
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=philadelphia,usa&units=imperial&APPID=99ddde24d75f18e8da53fbebabbdd073`)
     .then(resp => resp.json())
     .then(data => {
         day1.textContent = displayWeekday(data.list[0].dt_txt);
-        day1Temp.textContent = data.list[0].main.temp_max + " / " + data.list[0].main.temp_min + "°";
+        //day1Temp.textContent = data.list[0].main.temp_max + " / " + data.list[0].main.temp_min + "°";
+        // day1Icon.src = "https://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + ".png";
         day2.textContent = displayWeekday(data.list[7].dt_txt);
-        day2Temp.textContent = data.list[7].main.temp_max + " / " + data.list[7].main.temp_min + "°";
+        //day2Temp.textContent = data.list[7].main.temp_max + " / " + data.list[7].main.temp_min + "°";
+        //day2Icon.src = "https://openweathermap.org/img/wn/" + data.list[7].weather[0].icon + ".png";
         day3.textContent = displayWeekday(data.list[15].dt_txt);
-        day3Temp.textContent = data.list[15].main.temp_max + " / " + data.list[15].main.temp_min + "°";
+        //day3Temp.textContent = data.list[15].main.temp_max + " / " + data.list[15].main.temp_min + "°";
+        //day3Icon.src = "https://openweathermap.org/img/wn/" + data.list[15].weather[0].icon + ".png";
         day4.textContent = displayWeekday(data.list[23].dt_txt);
-        day4Temp.textContent = data.list[23].main.temp_max + " / " + data.list[23].main.temp_min + "°";
+        //day4Temp.textContent = data.list[23].main.temp_max + " / " + data.list[23].main.temp_min + "°";
+        //day4Icon.src = "https://openweathermap.org/img/wn/" + data.list[23].weather[0].icon + ".png";
         day5.textContent = displayWeekday(data.list[31].dt_txt);
-        day5Temp.textContent = data.list[31].main.temp_max + " / " + data.list[31].main.temp_min + "°";
+        //day5Temp.textContent = data.list[31].main.temp_max + " / " + data.list[31].main.temp_min + "°";
+        //day5Icon.src = "https://openweathermap.org/img/wn/" + data.list[31].weather[0].icon + ".png";
     });
 }
 get5DayWeather();
@@ -70,13 +104,8 @@ function getWeatherCelsius(){
     fetch("https://api.openweathermap.org/data/2.5/weather?q=philadelphia,usa&units=metric&APPID=99ddde24d75f18e8da53fbebabbdd073")
     .then(resp => resp.json())
     .then(data => {
-        // Location.textContent = data.name + "," + data.sys.country;
-        // Icon.textContent = data.weather.icon;
         temp.textContent = data.main.temp + "°C";
         feelsLike.textContent = "Feels Like: " + data.main.feels_like + "°C";
-        // wind.textContent = "Wind: " + data.wind.speed + " mph";
-        // humidity.textContent = "Humidity: " + data.main.humidity + "%";
-        // desc.textContent = data.weather[0].main;
     });
 }
 
@@ -99,26 +128,47 @@ function getWeatherInput() {
     .then(resp => resp.json())
     .then(data => {
         Location.textContent = data.name + "," + data.sys.country;
-        Icon.textContent = data.weather.icon;
+        mainIcon.src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
         temp.textContent = data.main.temp + "°F";
         feelsLike.textContent = "Feels Like: " + data.main.feels_like + "°F";
         wind.textContent = "Wind: " + data.wind.speed + " mph";
         humidity.textContent = "Humidity: " + data.main.humidity + "%";
         desc.textContent = data.weather[0].main;
+
+        lat2 = data.coord.lat;
+        lon2 = data.coord.lon;
+
+        weatherOneCall2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat2}&lon=${lon2}&exclude=hourly,minutely&units=imperial&appid=99ddde24d75f18e8da53fbebabbdd073`
+
+
+        getOneCall2(weatherOneCall2);
     });
 }
+
+function getOneCall2(weatherOneCall2){
+    fetch(weatherOneCall2)
+    .then (resp => resp.json())
+    .then (data => {
+        day1Temp.textContent = data.daily[1].temp.max + " / " + data.daily[1].temp.min + "°";
+        day1Icon.src = "https://openweathermap.org/img/wn/" + data.daily[1].weather[0].icon + ".png";
+        day2Temp.textContent = data.daily[2].temp.max + " / " + data.daily[2].temp.min + "°";
+        day2Icon.src = "https://openweathermap.org/img/wn/" + data.daily[2].weather[0].icon + ".png";
+        day3Temp.textContent = data.daily[3].temp.max + " / " + data.daily[3].temp.min + "°";
+        day3Icon.src = "https://openweathermap.org/img/wn/" + data.daily[3].weather[0].icon + ".png";
+        day4Temp.textContent = data.daily[4].temp.max + " / " + data.daily[4].temp.min + "°";
+        day4Icon.src = "https://openweathermap.org/img/wn/" + data.daily[4].weather[0].icon + ".png";
+        day5Temp.textContent = data.daily[5].temp.max + " / " + data.daily[5].temp.min + "°";
+        day5Icon.src = "https://openweathermap.org/img/wn/" + data.daily[5].weather[0].icon + ".png";
+    })
+}
+
 
 function getWeatherInputCelsius() {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&APPID=99ddde24d75f18e8da53fbebabbdd073`)
     .then(resp => resp.json())
     .then(data => {
-        // Location.textContent = data.name + "," + data.sys.country;
-        // Icon.textContent = data.weather.icon;
         temp.textContent = data.main.temp + "°C";
         feelsLike.textContent = "Feels Like: " + data.main.feels_like + "°C";
-        // wind.textContent = "Wind: " + data.wind.speed + " mph";
-        // humidity.textContent = "Humidity: " + data.main.humidity + "%";
-        // desc.textContent = data.weather[0].main;
     });
 }
 
@@ -135,24 +185,9 @@ function get5DayWeatherInput() {
     .then(resp => resp.json())
     .then(data => {
         day1.textContent = displayWeekday(data.list[0].dt_txt);
-        day1Temp.textContent = data.list[0].main.temp_max + " / " + data.list[0].main.temp_min + "°";
-        day2Temp.textContent = data.list[7].main.temp_max + " / " + data.list[7].main.temp_min + "°";
         day2.textContent = displayWeekday(data.list[7].dt_txt);
-        day3Temp.textContent = data.list[15].main.temp_max + " / " + data.list[15].main.temp_min + "°";
         day3.textContent = displayWeekday(data.list[15].dt_txt);
-        day4Temp.textContent = data.list[23].main.temp_max + " / " + data.list[23].main.temp_min + "°";
         day4.textContent = displayWeekday(data.list[23].dt_txt);
-        day5Temp.textContent = data.list[31].main.temp_max + " / " + data.list[31].main.temp_min + "°";
         day5.textContent = displayWeekday(data.list[31].dt_txt);
     });
 }
-
-
-function getLatLonWeather() {
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat1}&lon=${lon1}&appid=99ddde24d75f18e8da53fbebabbdd073`)
-    .then(resp => resp.json())
-    .then(data =>{
-        console.log(data)
-    })
-}
-getLatLonWeather();
